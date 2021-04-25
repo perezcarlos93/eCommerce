a35dfa69bbbcc288f49c68d7ccc08e70c7974707
Carlos <perez.carlos_a@yahoo.com>
2021-04-25 16:28:34 -0700

    removed unnecessary files

--- models/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	Team: require('./team'),
-	loginData: require('./loginData'),
-	Amenities: require('./parkAmenities'),
-	Comments: require('./parkComment'),
-	Photos: require('./PostPics'),
-	ProfilePhoto: require('./ProfilePics')
-};
